ba29a790f1cdccfc6616e592c7b8f64b5619fb1f
isliusar <isliusar@liaisonedu.com>
2023-06-01 11:59:06 +0300

    Added group option to task generator

--- lib/generators/task/task_generator.rb
+++ lib/generators/task/task_generator.rb
@@ -11,6 +11,11 @@ module AfterParty
         type: :string,
         description: 'Include a description'
       )
+      class_option(
+        :group,
+        type: :string,
+        group: 'available before_migrate|before_load|after_load'
+      )
 
       def copy_deploy_task
         template(
@@ -30,6 +35,16 @@ module AfterParty
         return line + file_name if options.description.blank?
 
         line + options.description
+      end
+
+      def task_group
+        return 'before_migrate' unless available_groups.include?(options.group.to_s)
+
+        options.group
+      end
+
+      def available_groups
+        ['before_migrate', 'before_load', 'after_load']
       end
 
       def timestamp
